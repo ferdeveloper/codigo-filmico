64d01c2ae2165ac31b148c8677c63e4e6aba4b19
Fernando Arenas Alapont <fer@MacBook-Pro-de-Fernando.local>
2023-10-22 22:29:43 +0200

    Adding episode component.

---
+++ src/components/EpisodeItem/EpisodeItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="EpisodeItem">
+    <div class="EpisodeItem__header">
+      <img
+        class="EpisodeItem__image"
+        :src="require(`@/assets/${image}`)"
+        :alt="`{title}`"
+      >
+      <div>
+        <h3>{{title}}</h3>
+        <h4>{{version}}</h4>
+      </div>
+    </div>
+    <p>{{description}}</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EpisodeItem',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    version: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    spotifyLink: {
+      type: String,
+      required: true,
+    },
+    applePodcastLink: {
+      type: String,
+      required: true,
+    },
+    youtubeLink: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="sass">
+    @import './EpisodeItem'
+</style>
---
+++ src/views/EpisodeListView/EpisodeListView.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable max-len -->
+<!-- eslint-disable vuejs-accessibility/iframe-has-title -->
+<template>
+  <div class="EpisodeListView">
+    <div class="EpisodeListView__content">
+      <EpisodeItem
+        title="Steve Jobs (2015)"
+        version="v1.1.0"
+        image="steve_jobs.jpg"
+        description="En este episodio hablamos sobre la película Steve Jobs (2015) y nos enfrascamos en una pequeña batalla con el único de nuestros colaboradores que esgrimía un móvil con SO Android. También podéis escuchar al gran Antonio Huerta que nos ha acompañado en este primer programa."
+        spotifyLink=""
+        applePodcastLink=""
+        youtubeLink=""
+      >
+        <iframe
+          style="border-radius:12px"
+          src="https://open.spotify.com/embed/episode/28MOONOnGs1sx0T4mCaEIg?utm_source=generator&theme=0"
+          width="100%"
+          height="152"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy">
+        </iframe>
+      </EpisodeItem>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import EpisodeItem from '@/components/EpisodeItem/EpisodeItem.vue';
+
+export default defineComponent({
+  name: 'EpisodeListView',
+  components: {
+    EpisodeItem,
+  },
+});
+</script>
+
+<style scoped lang="sass">
+    @import './EpisodeListView'
+</style>
