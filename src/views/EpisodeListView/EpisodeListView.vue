9e24a443c0908247a40f8f4314f40e15a40aa67d
Fernando Arenas Alapont <fer@MacBook-Pro-de-Fernando.local>
2023-10-29 20:37:24 +0100

    Space added for test.

--- src/views/EpisodeListView/EpisodeListView.vue
+++ src/views/EpisodeListView/EpisodeListView.vue
@@ -9,7 +9,7 @@
       title="Steve Jobs (2015)"
       version="v1.1.0"
       image="steve_jobs_miniature.jpg"
-      description="En este episodio hablamos sobre la película Steve Jobs (2015) y nos enfrascamos en una pequeña batalla con el único de nuestros colaboradores que esgrimía un móvil con SO Android. También podéis escuchar al gran Antonio Huerta que nos ha acompañado en este primer programa."
+      description="En este episodio hablamos sobre la película Steve Jobs (2015) y nos enfrascamos en una pequeña batalla con el único de nuestros colaboradores que esgrimía un móvil con SO Android. También podéis escuchar al gran Antonio Huerta que nos ha acompañado en este primer programa. "
       spotifyLink="https://spotify.link/jmMuLwPckDb"
       applePodcastLink="https://podcasts.apple.com/es/podcast/v1-1-0-steve-jobs-2015/id1708606379?i=1000628838482"
       youtubeLink="https://www.youtube.com/watch?v=PkPRAOx6E1o&t=39s"
